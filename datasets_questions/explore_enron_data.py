91a99b56c36521d876449981d421f0df9978a097
Arin Greenwood <argwood@uchicago.edu>
2019-08-14 16:01:19 -0500

    completed regression project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,51 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+for key in (enron_data['SKILLING JEFFREY K'].keys()):
+    print(key)
+pois = 0
+salary_c = 0
+email_c = 0
+no_total_pay = 0
+poi_no_pay = 0
+for name in enron_data.keys():
+#    print(name)
+    if enron_data[name]["poi"]==1:
+        pois+=1
+    if not enron_data[name]['salary']=='NaN':
+        salary_c+=1
+    if not enron_data[name]['email_address']=='NaN':
+        email_c+=1
+    if enron_data[name]['total_payments']=='NaN':
+        no_total_pay+=1
+        if enron_data[name]["poi"]==1:
+            poi_no_pay+=1
 
+
+
+
+print(' ')
+print('There are {} POIs in the dataset'.format(pois))
+
+print('Total stock value for J. Prentice is {}'.format(enron_data['PRENTICE JAMES']['total_stock_value']))
+print('There are {} email messages from W. Colwell to a POI'.format(enron_data['COLWELL WESLEY']['from_this_person_to_poi']))
+print('Value of stock options exercised for J. K. Skilling is {}'.format(enron_data['SKILLING JEFFREY K']['exercised_stock_options']))
+
+
+
+print('Total payments for J. K. Skilling is {}'.format(enron_data['SKILLING JEFFREY K']['total_payments']))
+print('Total payments for A. Fastow is {}'.format(enron_data['FASTOW ANDREW S']['total_payments']))
+print('Total payments for K. Lay is {}'.format(enron_data['LAY KENNETH L']['total_payments']))
+
+print('There are {} valid emails'.format(email_c))
+print('There are {} valid salaries'.format(salary_c))
+print('There are {} missing total payments, which is {}% of the dataset'.format(no_total_pay, 100*no_total_pay/len(enron_data.keys())))
+print('There are {} missing total payments from POIs, which is {}% of the POIs'.format(poi_no_pay, 100*poi_no_pay/pois))
+print(len(enron_data.keys()))
--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
-
 """
     Starter code for the regression mini-project.
-    
+
     Loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project).
@@ -10,53 +9,57 @@
     Draws a little scatterplot of the training/testing data
 
     You fill in the regression code where indicated:
-"""    
+"""
 
 
 import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
 
-### list the features you want to look at--first item in the 
+### list the features you want to look at--first item in the
 ### list will be the "target" feature
+
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+#features_list = ["bonus", "long_term_incentive"] #is long term incentive a better feature to use? -> yes, a bit
+
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
+test_color = "r"
 
 
 ### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
+### Please name it reg, so that the plotting code below picks it up and
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print('slope of regression: ', reg.coef_)
+print('intercept: ', reg.intercept_)
+print('r^2 score: ', reg.score(feature_test, target_test))
 
 
 
 ### draw the scatterplot, with color-coded training and testing points
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
-
-
 
 
 ### draw the regression line, once it's coded
@@ -64,7 +67,12 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+#what if we swap the test and training set since the latter doesn't have an outlier?
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color='b')
+print('slope of regression on testing set (without outlier): ', reg.coef_)
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
-plt.show()
+plt.savefig('finance_reg.png')
