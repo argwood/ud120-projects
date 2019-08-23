efb9bf81dfa04cad5958c9fa387ef5e55f097827
Arin Greenwood <argwood@uchicago.edu>
2019-08-23 18:28:54 -0500

    completed feature selection project

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,35 +41,48 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+#        temp_counter += 1
+        if temp_counter < 500:
             path = os.path.join('..', path[:-1])
-            print path
+            print(path)
             email = open(path, "r")
+            text = parseOutText(email)
+            print(name)
+            for sig_word in ['sara', 'shackleton', 'chris', 'germani', 'sshacklensf', 'cgermannsf']:
+                text = text.replace(sig_word, '')
+            word_data.append(text)
 
             ### use parseOutText to extract the text from the opened email
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
 
-            ### append the text to word_data
+            if name == 'sara':
+                from_data.append('0')
+            elif name == 'chris':
+                from_data.append('1')
+                ### append the text to word_data
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
 
 
             email.close()
-
-print "emails processed"
+print(word_data[152])
+print("emails processed")
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
 
 
 
 ### in Part 4, do TfIdf vectorization here
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words='english')
+x = vectorizer.fit_transform(word_data)
+vocab_list = vectorizer.get_feature_names()
+print('There are {} different words'.format(len(vocab_list)))
 
